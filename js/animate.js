165a27a38066070782e3221c2a6043831f1067cb
michaeljohnlum <michael.johnlum@gmail.com>
2013-06-16 16:40:44 +0100

    New Update

---
+++ js/animate.js
@@ -0,0 +1,19 @@
+$(function(){
+        $("#nav-portfolio").click(function () {
+            $("#logo").animate({
+            width:'70px',
+            left:'5%',
+            marginLeft: '0px',
+            top:'10px',
+            marginTop:'0px'
+         },500,'swing');
+               
+            $("#title").animate({
+            width:'250px',
+            left:'5%',
+            marginLeft: '80px',
+            top:'45px',
+            marginTop:'0px'
+         },500,'swing')   
+        });
+})();
---
+++ js/scroll.js
@@ -0,0 +1,106 @@
+(function() {
+  // http://www.zachstronaut.com/posts/2009/01/18/jquery-smooth-scroll-bugs.html
+  var scrollElement = 'html, body';
+  var $scrollElement;
+
+  $(function() {
+    $('html, body').each(function () {
+      var initScrollLeft = $(this).attr('scrollLeft');
+      
+      $(this).attr('scrollLeft', initScrollLeft + 1);
+      if ($(this).attr('scrollLeft') == initScrollLeft + 1) {
+        scrollElement = this.nodeName.toLowerCase();
+        $(this).attr('scrollLeft', initScrollLeft);
+        return false;
+      }    
+    });
+    $scrollElement = $(scrollElement);
+  });
+
+  /* Smooth scrolling of links between panels */
+  $(function() {
+    var $panels = $('.panel');
+
+    $panels.each(function() {
+      var $panel = $(this);
+      var hash = '#' + this.id;
+
+      $('a[href="' + hash + '"]').click(function(event) {
+        $scrollElement.stop().animate({
+          scrollLeft: $panel.offset().left
+        }, 500, 'swing', function() {
+          window.location.hash = hash;
+        });
+
+        event.preventDefault();
+      });
+    });
+  });
+
+ 
+  /* Force snap to panel on resize. */
+  $(function() {
+    var $window = $(window);
+    var timer;
+
+    $window.resize(function() {
+      window.clearTimeout(timer);
+      timer = window.setTimeout(function() {
+        var hash = window.location.hash ? window.location.hash : '#intro';
+
+        $scrollElement.stop().animate({
+          scrollLeft: $(hash).offset().left
+        }, 200);
+      }, 100);
+    });
+  });
+
+  /* Fix scroll snapping during browser finds */
+  $(function() {
+    var $window = $(window);
+    var timer;
+
+    /* Most finds will scroll a single panel. */
+    var scrollToPanel = function(panel) {
+      $scrollElement.scrollLeft($(panel).offset().left);
+    };
+
+    /* Others will scroll between panels but not cause a panel scroll */
+    var scrollToClosestPanel = function() {
+      var currentScroll = $window.scrollLeft();
+      var panelOffsets = $.map($('.panel').get(), function(el) {
+        return $(el).offset().left;
+      });
+      var closestOffset = 0;
+      var closestDistance = 99999999;
+
+      $.each(panelOffsets, function(i, offset) {
+        var offsetDistance = Math.abs(currentScroll - offset);
+        if(offsetDistance < closestDistance) {
+          closestDistance = offsetDistance;
+          closestOffset = offset;
+        }
+      });
+      $scrollElement.scrollLeft(closestOffset);
+    };
+
+    $('.panel').scroll(function() {
+      window.clearTimeout(timer);
+      timer = window.setTimeout(scrollToPanel, 50, this);
+    });
+
+    /* 50ms is enough time to let the animation between panels do its
+       thing without triggering this debounced panel snap. */
+    $window.scroll(function() {
+      window.clearTimeout(timer);
+      timer = window.setTimeout(scrollToClosestPanel, 50);
+    });
+  });
+
+
+
+})();
+
+
+
+
